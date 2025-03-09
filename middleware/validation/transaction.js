0a1ef861b4db6a01398bcc3a84b371c29785d62b
yash <yash.goodluck4@gmail.com>
2025-03-09 13:39:25 +0530

    remove dev

--- middleware/validation/transaction.js
+++
@@ -1,9 +0,0 @@
-const { body } = require("express-validator");
-
-const createTransactionValidation = [
-  body("amount").isFloat().withMessage("Amount must be a positive number"),
-];
-
-module.exports = {
-  createTransactionValidation,
-};
